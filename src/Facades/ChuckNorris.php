8c4ee9dab519d804f444b6ee3a445b44392444ef
Daniel Lucas <daniel.chris.lucas@gmail.com>
2020-10-27 00:09:04 +0100

    Add Laravel support

---
+++ src/Facades/ChuckNorris.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DanielChrisLucas\ChuckNorrisJokes\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class ChuckNorris extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'chuck-norris';
+    }
+}
